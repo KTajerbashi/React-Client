5537a91f698cdffb300de5b2be7274144b3b51a4
KTajerbashi <kamrantajerbashi@gmail.com>
2025-12-06 13:56:02 +0330

    #Feature Clean Solution

--- src/Utilities/redux.js
+++
@@ -1,7 +0,0 @@
-import { connect } from 'react-redux';
-
-export const connectRedux = (component, mapStateToProps) => {
-    return connect(mapStateToProps, null, null, {
-        forwardRef: true
-    })(component);
-};
