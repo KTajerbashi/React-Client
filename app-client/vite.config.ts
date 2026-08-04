49c6759009615bcc1f3d9bf0431df0f902b4448e
KTajerbashi <kamrantajerbashi@gmail.com>
2026-08-04 23:14:02 +0330

    #Feature Add Docker-Compose to project and now can clone repo and then "run npm i" in dir app-client and after install node_module then back to root of project and call "docker compose up -d"

--- app-client/vite.config.ts
+++ app-client/vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    strictPort: true,
+    port: 3000,  // برای development
   },
   preview: {
-    port: 3000,
-    strictPort: true,
+    port: 3000,  // برای production preview
   },
-});
+})
